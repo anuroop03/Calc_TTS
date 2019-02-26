b4b0216cb2aaccb1b8da56d489cf6debe1296617
anuroop03 <anuroop.divakaran@gmail.com>
2019-02-26 19:11:10 +0530

    Calc

--- proj.ino
+++ proj.ino
@@ -15,9 +15,25 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
 
+int prevans=0;
+int k=0;
+int flag=0;
+int ans=0;
+char str[4];
+int count = 0;
+int num1,num2;
 const int aPin[] ={ A0,A1,A2,A3,A6}; 
 int sensorValue = 0; 
-
+int answer()
+{
+  switch(str[1])
+  {
+    case '+': return num1+num2;
+    case '-': return num1-num2;
+    case '*': return num1*num2;
+    case '/': return num1/num2;
+  }
+}
 int power(int b, int e)
 {
   int val=1;
@@ -32,87 +48,40 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
     
 }
-
-int ans=0;
-int opp1=0;
-int opp2=0;
-int op;
-void loop() {
-  // put your main code here, to run repeatedly:
-String an;
-ans = 0;
-display.clearDisplay();
-for(int i=0;i<5;i++)
+int sensor()
+{
+  int a,sensorValue;
+  a=0;
+  for(int i=0;i<5;i++)
 {
   sensorValue = analogRead(aPin[i]);
   if(sensorValue<500)
-  ans+=power(2,i);
+  a+=power(2,i);
 }
-if(ans<10)
-{
-  an=ans;
-  opp1=ans;
-         
-  }
-
+if(a&&a<10)
+return a;
 else
-switch(ans)
-{
-  case 10: an='+';
-           break;
-  case 11: an='-';
-           break;
-  case 12: an='*';
-           break;
-  case 13: an='/';
-           break;
-  case 14: an="sin()";
-           break;
-  case 15: an="cos()";
-           break;
-  case 16: an="tan()";
-           break;
-  default: an= ans;            
+sensor();
 }
-display.setTextSize(4);
+
+void loop() {
+  // put your main code here, to run repeatedly:
+display.clearDisplay();
+display.setTextSize(2);
 display.setTextColor(WHITE);
 display.setCursor(0,0);
-display.println(an);
+display.println("first Number:");
 display.display();
+num1 = sensor();
 delay(1000);
-op=ans;
-opp2=0;
-if(op>9&&opp1);
-{
-  switch(op)
-  {
-    case 10:
-    case 11:
-    case 12:
-    case 13:for(int i=0;i<5;i++)
-           {
-            sensorValue = analogRead(aPin[i]);
-            if(sensorValue<500)
-            ans+=power(2,i);
-           }
-           if(ans<10)
-           opp2=ans;
-           break;
-  case 14: display.println(sin(opp1));
-           break;
-  case 15: display.println(cos(opp1));
-           break;
-  case 16: display.println(tan(opp1));                   
-  }     
-  if(opp2)
-  {
-    if(op==10)
-    display.println(opp1+opp2);
-    
-    if(op==11)
-    display.println(opp1-opp2);
-  
-  }
-//display.display();
-}
+display.println(num1);
+display.clearDisplay();
+display.setTextSize(2);
+display.setTextColor(WHITE);
+display.setCursor(0,0);
+display.println("Second Number:");
+num2=sensor();
+display.println(num1+num2);
+delay(1000);
+
 }
